f3838d81e4fc0bc62b88b0b27eb4310cb09faee0
Jonne Arjoranta <jonne.arjoranta@gmail.com>
2016-11-02 18:40:41 +0200

    Made the grammar more complex

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 var tracery = require('tracery-grammar');
 
 var spyMissions = require('./grammar.json');
+var namelist = require('./names.json');
 
 var grammar = tracery.createGrammar(spyMissions);
+var names = tracery.createGrammar(namelist);
 
 grammar.addModifiers(tracery.baseEngModifiers); 
 
-console.log(grammar.flatten('#origin#'));
+var result = grammar.flatten('#origin#');
+
+var output = 
+	"TOP SECRET \n\n" + 
+	"Memorandum for: " + names.flatten('#fullname#') + "\n\n" + 
+	"December 12th, 1943 \n\n" +
+	result + "\n\n" +
+	"Signed by,\n" + names.flatten('#fullname#') + 
+	"\n\nTOP SECRET";
+
+console.log(output);

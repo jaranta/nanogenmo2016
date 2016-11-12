7cd1dd3d13033e0223d48ea382eb6455b9349b3c
Jonne Arjoranta <jonne.arjoranta@gmail.com>
2016-11-12 15:25:23 +0200

    More complex grammar and optional elements

--- main.js
+++ main.js
@@ -21,18 +21,27 @@ function getNames(min, max) {
     return namesText;
 }
 
+function getAttachements(min, max) {
+    var attachementText = "";
+    var attachementNumber = Math.floor((Math.random() * max) + min);
+    for (i = 0; i < attachementNumber; i++) {
+    attachementText += "* " + grammar.flatten('#attachement#') + " (" + (i + 1) + ")" + "\n";
+    }
+    return attachementText;
+}
+
 var output = 
 	"TOP SECRET \n\n" +
 	"Memorandum for:\n" + getNames(1,5) + "\n\n" + 
-    randomDate(new Date(1900, 0, 1), new Date()).toDateString() + "\n\n" +
-    "Subject: PROJECT " + grammar.flatten('#codenames#') + "\n\n" +
-    grammar.flatten('#background#') + "\n\n" +
+	randomDate(new Date(1900, 0, 1), new Date()).toDateString() + "\n\n" +
+	"Subject: Operation report for PROJECT " + grammar.flatten('#codenames#') + "\n\n" +
+	grammar.flatten('#background#') + "\n\n" +
 	grammar.flatten('#mission#') + "\n\n" +
 	grammar.flatten('#result#') + "\n\n" +
 	grammar.flatten('#conclusion#') + "\n\n" +
-	grammar.flatten('#attachements#') + "\n\n" +
+	"See the following attachements:" + "\n" + getAttachements(1,3) + "\n\n" +
 	"Signed by,\n" + getNames(1,1) + "\n\n" +
-    "Classified by " + grammar.flatten('#agencies#') + "\n\n" +
+	"Classified by " + grammar.flatten('#agencies#') + "\n\n" +
 	"TOP SECRET";
 
 console.log(output);
